f5549107c518d15b58466a72fefba4dc0c4d969f
Manmohan <manmohanverma111@gmail.com>
2018-05-03 10:07:17 +0530

    added delete request

--- server.js
+++ server.js
@@ -38,13 +38,30 @@ app.post('/todos', function (req,res) {
 	var body = req.body;
 	body = _.pick(body,	'description','completed');
 	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length===0){
-		res.status(400).send();
+		res.status(404).send();
+	} else {
+		body.description = body.description.trim();
+		body.id = todoNextId++;
+		todos.push(body);
+		res.json(body);
 	}
-	body.description = body.description.trim();
-	body.id = todoNextId++;
-	todos.push(body);
-	res.json(body);
 });
+
+app.delete('/todos/:id', function (req,res){
+	var todoId = parseInt(req.params.id,10);
+	var match = _.findWhere(todos,{id:todoId});
+	if(!match){
+		res.status(404).json({"error": "no todo found with that id"});
+	}else{
+		todos = _.without(todos,match);
+		res.json(match);
+	}
+	
+});
+
+
+
+
 
 app.listen(PORT, function (){
 	console.log('Express listening on port '+ PORT + '!');
