33589b65642e3978f18cd94fbecf7c691a37ee1b
Manmohan <manmohanverma111@gmail.com>
2018-06-12 18:57:17 +0530

    last message

--- server.js
+++ server.js
@@ -121,7 +121,12 @@ app.post('/users/login', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 
 	db.user.authenticate(body).then(function(user) {
-		res.header('Auth', user.generateToken('authentication')).json(user.toPublicJSON());
+		var token = user.generateToken('authentication');
+		if(!token){
+			res.status(401).send(e);	
+		} else{
+			res.header('Auth', token).json(user.toPublicJSON());
+		}
 	}, function(e) {
 		res.status(401).send(e);
 	});
